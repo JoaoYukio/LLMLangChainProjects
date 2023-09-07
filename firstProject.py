981a6564046d066897447f6619ef190c06310ef2
João Vitor Yukio Bordin Yamashita <yukiobr98@gmail.com>
2023-09-06 22:00:03 -0300

    Add first test

--- firstProject.py
+++ firstProject.py
@@ -1,5 +1,22 @@
 import os
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
 
 if __name__ == "__main__":
-    print("Hello LangChain")
-    print(os.environ["OPENAI_API_KEY"])
+    summary_template = """
+        Dada a informação {information} que foi fornecida, gostaria que criasse:
+        1. Um breve resumo
+        2. Fatos interessantes
+    """
+
+    promt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=promt_template)
+
+    print(chain.run(information="O Brasil é um país localizado na América do Sul."))
